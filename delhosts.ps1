378c3de130adf6e2f44463a9c0214f609c919cf6
ianklatzco <iklatzco@gmail.com>
2014-03-06 00:28:56 -0600

    added some code that allows it to work with any line length

--- delhosts.ps1
+++ delhosts.ps1
@@ -1,9 +1,13 @@
-﻿#load into object | remove last two lines | write new file
-$op = get-content C:\Windows\system32\drivers\etc\hosts | Select-Object -first 32
+﻿$i = 0
+$length = get-content C:\Windows\system32\drivers\etc\hosts | Measure-Object -Line
+
+#load into object | remove last two lines | write new file
+$op = get-content C:\Windows\system32\drivers\etc\hosts | Select-Object -first ($length.lines-2)
+#.lines takes the value so you don't get the whole object
 write-output $op | out-file -Encoding UTF8 C:\Windows\system32\drivers\etc\hosts
 
 
-scraps
+#scraps
 #[IO.File]::WriteAllText("C:\Windows\system32\drivers\etc\hosts2",($output+"`n"))
 #out-file C:\Windows\system32\drivers\etc\hosts2
 #| add-content C:\Windows\system32\drivers\etc\hosts2 
