0cc466119083364d7aad4a89bd5ed9b7c8313042
ianklatzco <iklatzco@gmail.com>
2014-03-06 00:06:14 -0600

    initial commit

---
+++ addhosts.ps1
@@ -0,0 +1,1 @@
+﻿ac -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "127.0.0.1 hostname1`n127.0.0.1 hostname2"
---
+++ delhosts.ps1
@@ -0,0 +1,13 @@
+﻿#load into object | remove last two lines | write new file
+$op = get-content C:\Windows\system32\drivers\etc\hosts | Select-Object -first 32
+write-output $op | out-file -Encoding UTF8 C:\Windows\system32\drivers\etc\hosts
+
+
+scraps
+#[IO.File]::WriteAllText("C:\Windows\system32\drivers\etc\hosts2",($output+"`n"))
+#out-file C:\Windows\system32\drivers\etc\hosts2
+#| add-content C:\Windows\system32\drivers\etc\hosts2 
+#select-object -skip 33
+#out-file C:\Windows\system32\drivers\etc\hosts
+#get-content C:\Users\Ian\Desktop | select-object | out-file C:\Windows\system32\drivers\etc\hosts
+#For ($i=1; $i -gt 1; $i--)  { get-content C:\Windows\system32\drivers\etc\hosts | Select-Object -Skip $i }
